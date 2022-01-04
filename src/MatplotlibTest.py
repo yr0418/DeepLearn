5607c9b6a1e47d67a1332e3e9a4cd2348acf91d9
yr0418 <1933352594@qq.com>
2022-01-04 23:21:27 +0800

    深度学习中，NumPy 库和 Matplotlib 库的基本使用

---
+++ src/MatplotlibTest.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+import math
+
+if __name__ == '__main__':
+    x = np.arange(0, math.pi, 0.1)    # 生成从 0 到 π 的数据，步长为 0.1。即：0，0.1,0.2...
+    y1 = np.sin(x)
+    y2 = np.cos(x)
+
+    # 绘制图形
+    plt.plot(x, y1, label="sin")
+    plt.plot(x, y2, linestyle="--", label="cos")    # 使用虚线绘制
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title("sin & cos")
+    plt.legend()  # 显示两个图形分别用什么线表示
+    plt.show()    # 显示图形
+
+    img = imread('../dataSet/testImg/T1.jpg')    # 读取图片
+    plt.imshow(img)
+    plt.show()
